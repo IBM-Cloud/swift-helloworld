9b9f931d12bea9457d6dcdea3a1a7ec46adb3f3d
Christian Compton <cbcompto@us.ibm.com>
2018-08-03 09:50:31 -0500

    Swift 4.1.3 & documentation updates

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 /**
- * Copyright IBM Corporation 2016,2017
+ * Copyright IBM Corporation 2016,2017,2018
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ let package = Package(
             targets: ["Server", "Utils"])
     ],
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", .upToNextMajor(from: "7.1.0")),
+        .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", .upToNextMajor(from: "8.0.0")),
         .package(url: "https://github.com/IBM-Swift/BlueSocket.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMajor(from: "1.0.0"))
     ],
